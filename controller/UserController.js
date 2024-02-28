a9555f2022742a414a78beec084a3349628ecf31
abhinavjha29 <abhinav_29@outlook.com>
2024-02-28 22:58:30 +0530

    used sql for joining

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,25 +1,36 @@
-const UserModel = require("../models/userTable");
-const UserDetailModel = require("../models/user_DetailsTable");
+const sequelize = require("../util/dbconnection");
 
 const postUserDetail = async (req, res) => {
   try {
     const { email } = req.body;
-    const user = await UserModel.findOne({
-      where: { email },
-      include: [{ model: UserDetailModel }],
+    const query = `
+      SELECT 
+        ut.*, udt.*
+      FROM 
+        users AS ut
+      LEFT JOIN 
+        user_details AS udt ON ut.userId = udt.UserUserId
+      WHERE 
+        ut.email = :email
+    `;
+
+    const [results] = await sequelize.query(query, {
+      replacements: { email },
+      type: sequelize.QueryTypes.SELECT,
     });
 
-    if (!user) {
+    if (!results.length) {
       return res.status(404).json({ message: "Wrong Email" });
-    } else {
-      return res.status(200).json({
-        message: "User found",
-        data: user,
-      });
     }
+
+    return res.status(200).json({
+      message: "User found",
+      data: results,
+    });
   } catch (error) {
     console.error("Error retrieving user details:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
 module.exports = { postUserDetail };
